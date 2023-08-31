19ac02ebcb081a581cbf2fc48d8b7e36132eae3e
Serhii-Myroshnychenko <serzh.miroshnichenko.122@gmail.com>
2023-08-31 15:13:04 +0300

    Created Basket microservice

---
+++ Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,38 @@
+﻿using Basket.API.Entities;
+using Basket.API.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Basket.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BasketController : ControllerBase
+{
+    private readonly IBasketRepository _basketRepository;
+    public BasketController(IBasketRepository basketRepository)
+    {
+        _basketRepository = basketRepository;
+    }
+    [HttpGet("{username}",Name = "GetBasket")]
+    [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+    public async Task<ActionResult<ShoppingCart>> GetBasket(string username)
+    {
+        return Ok(await _basketRepository.GetBasket(username));
+    }
+
+    [HttpPost]
+    [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+    public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody]ShoppingCart basket)
+    {
+        return Ok(await _basketRepository.UpdateBasket(basket));
+    }
+    [HttpDelete("{username}", Name = "DeleteBasket")]
+    [ProducesResponseType(typeof(void), (int)HttpStatusCode.OK)]
+    public async Task<ActionResult<ShoppingCart>> DeleteBasket(string username)
+    {
+        await _basketRepository.DeleteBasket(username);
+        return Ok();
+    }
+}
---
+++ Services/Basket/Basket.API/Program.cs
@@ -0,0 +1,26 @@
+using Basket.API.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetValue<string>("CacheSettings:ConnectionString");
+});
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapControllers();
+
+app.Run();
