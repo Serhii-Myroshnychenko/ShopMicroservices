858bf844a751b66e66a694afb971ddf879a15b16
Serhii-Myroshnychenko <serzh.miroshnichenko.122@gmail.com>
2023-09-07 16:15:45 +0300

    Created Discount.grpc service

---
+++ ShopMicroservises/Services/Discount/Discount.Grpc/Mapper/DicountProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Discount.Grpc.Entities;
+using Discount.Grpc.Protos;
+
+namespace Discount.Grpc.Mapper;
+
+public class DicountProfile : Profile
+{
+    public DicountProfile()
+    {
+        CreateMap<Coupon, CouponModel>().ReverseMap();
+    }
+}
